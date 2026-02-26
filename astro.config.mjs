70e606906b74a1f3a71e1c06e8474c48f5c7fb2e
PauloGuidolin <paulog162003@gmail.com>
2026-02-26 12:38:34 -0300

    integracion netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
-  output:"server"
+
+  output:"server",
+  adapter: netlify()
 });
