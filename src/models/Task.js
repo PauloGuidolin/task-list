ef53a8e37d64da8292d9372d0c44a335c72e1b88
PauloGuidolin <paulog162003@gmail.com>
2026-02-26 12:53:27 -0300

    arrgelo fecha

--- src/models/Task.js
+++ src/models/Task.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const TaskSchema = new mongoose.Schema({
   title: { type: String, required: true }, //titulo tarea
   status: { type: String, default: 'pendiente'}, //status tarea
-  createdAt: { type: Date, default: Date.now.toLocalString } //fechaCreacion
+  createdAt: { type: Date, default: Date.now} //fechaCreacion
 });
 
 export default mongoose.models.Task || mongoose.model('Task', TaskSchema); 
